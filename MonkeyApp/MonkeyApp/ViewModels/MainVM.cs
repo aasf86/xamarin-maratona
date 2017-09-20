7be3dd39d076f9bbbec3ab7e3848f57ec2972140
Anderson Fernandes <aasf86@gmail.com>
2017-09-20 19:07:32 -0300

    initial

---
+++ MonkeyApp/MonkeyApp/ViewModels/MainVM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MonkeyApp.ViewModels
+{
+    public class MainVM : BaseVM
+    {
+        private string _searchTerm;       
+
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set
+            {
+                if (SetProperty(ref _searchTerm, value))
+                    SearchCommand.ChangeCanExecute();
+            }
+        }
+
+        public Command SearchCommand { get; }
+
+        public MainVM()
+        {
+            SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
+        }
+
+        bool CanExecuteSearchCommand(object arg)
+        {
+            return !string.IsNullOrWhiteSpace(SearchTerm);
+        }
+
+        async void ExecuteSearchCommand(object obj)
+        {
+            await Task.Delay(2000);
+
+            await App.Current.MainPage.DisplayAlert("MonkeyHubApp", $"Você pesquisou por '{SearchTerm}'.", "OK");
+
+            Debug.WriteLine($"Clicou em {DateTime.Now}");
+        }
+    }
+}
